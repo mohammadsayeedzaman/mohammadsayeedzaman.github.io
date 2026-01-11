a87ed6b6ba22aec48e5a8eff6b689bbfaf5fc41d
mohammadsayeedzaman <mohammadsayeedzaman@gmail.com>
2026-01-12 02:15:20 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+﻿function activate() {
+    const code = document.getElementById("code").value;
+    const result = document.getElementById("result");
+
+    if (code === "1234") {
+        result.style.color = "green";
+        result.innerText = "Activation Successful ✅";
+    } else {
+        result.style.color = "red";
+        result.innerText = "Invalid Code ❌";
+    }
+}
